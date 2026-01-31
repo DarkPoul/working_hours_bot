712219948825fbda4029d5c49561bff97942e4a4
DarkPoul <poulpotter1@gmail.com>
2026-02-01 01:18:01 +0200

    Початковий комміт

---
+++ src/main/java/esvar/ua/working_hours_bot/WorkingHoursBotApplication.java
@@ -0,0 +1,13 @@
+package esvar.ua.working_hours_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingHoursBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingHoursBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esvar/ua/working_hours_bot/WorkingHoursBotApplicationTests.java
@@ -0,0 +1,13 @@
+package esvar.ua.working_hours_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingHoursBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
